d7805ad7ab2e64e439aed4d26ad6a9e85314123b
Marcus Ngugi <marcusngugi@gmail.com>
2024-03-27 14:10:43 -0700

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,55 +1,40 @@
 #!/usr/bin/python3
-
-"""Log Parsing Module
-Will contain script that reads stdin line by line and computes metrics:
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-Each 10 lines and after a keyboard interruption (CTRL + C),
-prints those statistics since the beginning:
-Total file size: File size: <total size>
-where is the sum of all previous (see input format above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear, don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
 """
-
+reads stdin line by line and computes metrics
+"""
 import sys
 
-metrics = {
-    'total_size': 0,
-    'status_codes': {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
-                     404: 0, 405: 0, 500: 0},
-    'line_count': 0
-}
-
+file_size = 0
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+i = 0
 try:
     for line in sys.stdin:
-        try:
-            parts = line.split()
-            if len(parts) >= 7:
-                status_code = int(parts[-2])
-                file_size = int(parts[-1])
-                metrics['total_size'] += file_size
-                if status_code in metrics['status_codes']:
-                    metrics['status_codes'][status_code] += 1
-                metrics['line_count'] += 1
+        tokens = line.split()
+        if len(tokens) >= 2:
+            a = i
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                if a == i:
+                    i += 1
+            except:
+                if a == i:
+                    continue
+        if i % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for key, value in sorted(status_tally.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
 
-                if metrics['line_count'] % 10 == 0:
-                    print("File size: {}".format(metrics['total_size']))
-                    for code in sorted(metrics['status_codes']):
-                        if metrics['status_codes'][code] > 0:
-                            print("{}: {}".format(
-                                code, metrics['status_codes'][code]
-                            ))
-
-        except ValueError:
-            continue
 except KeyboardInterrupt:
-    pass
-
-print("File size: {}".format(metrics['total_size']))
-for code in sorted(metrics['status_codes']):
-    if metrics['status_codes'][code] > 0:
-        print("{}: {}".format(code, metrics['status_codes'][code]))
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
